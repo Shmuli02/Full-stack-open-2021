72b5a193670da6237611a9d1b4d9183d20fada24
Shmuli02 <samuli.kaleva@gmail.com>
2021-04-27 12:58:24 +0000

    teht. 1.12-1.14

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Button = ({action,text}) => (
+    <button onClick={action}>{text}</button>
+)
+
+const Display = ({point}) => {
+  if (point<2) {
+    return (
+      <p>has {point} vote</p>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p>has {point} votes</p>
+  )
 }
 
-export default App;
+const App = () => {
+  const [points, setPoints] = useState(Array(6).fill(0))
+  const [selected, setSelected] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+  
+  const add_points = () => {
+    const copy = [ ...points ]
+    copy[selected] += 1
+    setPoints(copy)
+    
+  }
+  const most_voted = points.indexOf(Math.max(...points))
+  
+  const new_anecdotes = () => {
+    const random_int = Math.floor(Math.random() * anecdotes.length)
+    setSelected(random_int)
+  }
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <Display point={points[selected]} />
+      <br></br>
+      <Button action={add_points} text="Vote!"/>
+      <Button action={new_anecdotes} text="next anecdote"/>
+      
+      <h1>Anecdote with most votes</h1>
+      
+      {anecdotes[most_voted]}
+      <Display point={points[most_voted]} />
+    </div>
+  )
+}
+
+export default App
